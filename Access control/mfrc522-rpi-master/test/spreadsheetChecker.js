cffd9cdd4c1278008dd3ad899521fb6937c6230b
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-16 13:57:12 -0500

    Update spreadsheetChecker.js

--- Access control/mfrc522-rpi-master/test/spreadsheetChecker.js
+++ Access control/mfrc522-rpi-master/test/spreadsheetChecker.js
@@ -7,12 +7,38 @@ const getDataFromSheet = async function test (){
 
     const client = await auth.getClient();
     const googleSheets = google.sheets({ version: "v4", auth: client});
-    const spreadsheetId = "1XMSZRJNUllFxmb1unSxeeKXc7FFdT1XikAEPd5OSgrA";
-    const getRows = await googleSheets.spreadsheets.values.get({
+    // const spreadsheetId = "1XMSZRJNUllFxmb1unSxeeKXc7FFdT1XikAEPd5OSgrA";
+    const spreadsheetId = "1k3eZkkqm1bWA3lk8gUgfoR6Xpb2vVaX4iaqnizi5iDc";
+
+    const getBatch = await googleSheets.spreadsheets.values.batchGet({
         auth,
         spreadsheetId,
-        range: "Sheet1!A:A",    
+        ranges:['Authorizations']
         })
-    return getRows.data;
+    const getRow = async function(row){
+        return await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId,
+            range: `Authorizations!${1}:${1}`
+        });    
+    }
+
+    const getUsers = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: 'Authorizations!A2:A'
+    })
+    //console.log(JSON.stringify(getUsers.data, null, 2));
+    return {
+        // batch:JSON.stringify(getBatch.data, null, 2),
+        // row: JSON.stringify((await getRow()).data, null, 2),
+        // users:JSON.stringify(getUsers.data, null, 2)
+        batch:getBatch.data,
+        row: (await getRow()).data,
+        users: getUsers.data
+    };
 }
-module.exports = {getDataFromSheet};
+// exports.getDataFromSheet = getDataFromSheet;
+// exports.getRow = getDataFromSheet.getRow();
+ module.exports = {getDataFromSheet};
+//  module.exports.getRows = getDataFromSheet.getRow();
