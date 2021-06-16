e3b4542bbfd02e5987c85df50b62c0ce667a2a41
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-16 14:24:07 -0500

    Update controllingMachine.js

--- Access control/mfrc522-rpi-master/test/controllingMachine.js
+++ Access control/mfrc522-rpi-master/test/controllingMachine.js
@@ -4,3 +4,6 @@ exports.runMachine = function (){
 exports.stopMachine = function(){
     console.log('To be implemented, power off');
 };
+exports.noAccess = function(){
+    console.log("to be implemented, you don't have access")
+}
