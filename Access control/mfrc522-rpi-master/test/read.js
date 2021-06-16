51cc544f62d1522b9be2d17eddf261a93bb7c6cc
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-16 11:21:51 -0500

    Update read.js

--- Access control/mfrc522-rpi-master/test/read.js
+++ Access control/mfrc522-rpi-master/test/read.js
@@ -32,7 +32,8 @@ const loop = function (){
     }
     //# If we have the UID, continue
     const uid = response.data;
-    let UID =+ '' + uid[0].toString(16)+uid[1].toString(16)+uid[2].toString(16)+uid[3].toString(16);
+    let UID = '';
+    UID +=  (uid[0].toString(16)) += (uid[1].toString(16)) + (uid[2].toString(16))+ (uid[3].toString(16));
     console.log(UID);
     
     if (UID ==" ff 83 aa 29"){
