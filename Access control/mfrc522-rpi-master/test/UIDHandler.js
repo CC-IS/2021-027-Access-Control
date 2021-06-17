94c2c68eb1559c65a5e3237f7e509f52c1d507fa
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-17 15:11:08 -0500

    Create UIDHandler.js

---
+++ Access control/mfrc522-rpi-master/test/UIDHandler.js
@@ -0,0 +1,11 @@
+const control = require('./controllingMachine');
+
+class UIDHandler{
+    constructor(UID){
+        if (!UID){
+            console.log("Insert a card");
+        }
+        
+    }
+}
+exports.handler = UIDHandler;
