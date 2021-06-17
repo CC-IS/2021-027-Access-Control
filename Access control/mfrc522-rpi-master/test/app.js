d7cb9da39dd06d3bc14c0bc40a7f78819b1d68b6
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-17 13:54:38 -0500

    Update app.js

--- Access control/mfrc522-rpi-master/test/app.js
+++ Access control/mfrc522-rpi-master/test/app.js
@@ -32,8 +32,10 @@ const loop = function (result){
       progmode = true;
       console.log('Entered Programming Mode /n please input user card after 3 seconds');
       sleep(3000);
-      UID2bAdded = await read.read2bAddedUser();
-      await sheet.addUser(UID2bAdded,devNum);
+      UID2bAdded = read.read2bAddedUser().then (()=>{
+        sheet.addUser(UID2bAdded,devNum);
+      
+      });
       
     }
     let found = sheet.foundUser(result.values, UID);
