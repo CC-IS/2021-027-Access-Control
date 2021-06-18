d5a2ba79a232bd54916afc5f80975c803b5ed9fe
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-18 09:53:42 -0500

    Update spreadsheetChecker.js

--- Access control/mfrc522-rpi-master/test/spreadsheetChecker.js
+++ Access control/mfrc522-rpi-master/test/spreadsheetChecker.js
@@ -76,7 +76,7 @@
                 //  console.log(col);
                  this.changeCell(`${String.fromCharCode(65 + col)}${arr[1]+2}`,1);
                  console.log(`User ${UID} permitted access successfully to ${devName}`);
-                 this.authorize();
+                //  this.authorize();
                  
                  // change col to be 1
              }
