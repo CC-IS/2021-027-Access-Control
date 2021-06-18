837e4a58ef5b6c7522918fe6a05792787d9cbb81
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-18 10:40:44 -0500

    Update test.js

--- Access control/mfrc522-rpi-master/test/test.js
+++ Access control/mfrc522-rpi-master/test/test.js
@@ -1,7 +1,13 @@
-// const {getDataFromSheet} = require('./spreadsheetChecker');
-// const sheet = new getDataFromSheet();
+// // const {getDataFromSheet} = require('./spreadsheetChecker');
+// // const sheet = new getDataFromSheet();
 
-// sheet.addUser('this is UID', 1);
+// // sheet.addUser('this is UID', 1);
 
-Delay(5000);
-console.log("x");
+// Delay(5000);
+// console.log("x");
+
+const x = setInterval(() => {
+    console.log("running");
+}, 2000);
+
+// clearInterval(x);
