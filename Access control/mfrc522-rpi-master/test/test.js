0d63b4dfbe2f3a200b8e6aef8ed5c6361cdbeca9
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-18 11:49:09 -0500

    Update test.js

--- Access control/mfrc522-rpi-master/test/test.js
+++ Access control/mfrc522-rpi-master/test/test.js
@@ -15,6 +15,9 @@ const sheet = new getDataFromSheet();
 sheet.onReady = ()=>{
     console.log(sheet.isUser(230213));
     console.log(sheet.getIndex(230213));
+    sheet.hasAccess(2313,1).then((result)=>{
+        console.log(result);
+    })
 }
 
 
