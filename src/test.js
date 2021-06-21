8e9e195601b4125547eb8c29da9efba31e4888d1
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-21 16:02:40 -0500

    after implemeting arduino

--- src/test.js
+++ src/test.js
@@ -1,13 +1,15 @@
-// const {getDataFromSheet} = require ('./spreadsheetChecker.js');
+const {getDataFromSheet} = require ('./spreadsheetChecker.js');
 
-// const sheet = new getDataFromSheet();
+const sheet = new getDataFromSheet();
 
-const {HardwareControl} = require('./controller.js');
+// const {HardwareControl} = require('./controller.js');
 
-const ControllerInstance = new HardwareControl("test");
+// const ControllerInstance = new HardwareControl("test");
 
-console.log(ControllerInstance.get.mode());
+// console.log(ControllerInstance.get.mode());
 
-// sheet.isAdminPresent().then((result)=>{
-//     console.log(result.data.values[0][0]);
-// })
+
+
+sheet.getRow(1).then((result)=>{
+    console.log(result.data.values);
+})
