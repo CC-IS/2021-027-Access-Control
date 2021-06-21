98af0b6921219181451184ae35699f50ab2e8627
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-21 13:58:39 -0500

    reorganization

--- app.js
+++ app.js
@@ -1,18 +1,64 @@
-var { HardwareControl } = require('./src/controller.js');
+const config = require ('/boot/config.json');
+const {getDataFromSheet} = require('./src/spreadsheetChecker');
+const {readClass} = require('./src/read');
+const control = require('./src/controllingMachine');
+const { managedidentities } = require('googleapis/build/src/apis/managedidentities');
+// const devName="CCIS-VBS-001";
+const devName = config.device;
+const adminUIDs = config.admins;
+// let adminUIDs =["c66759a5"];
+const sheet = new getDataFromSheet();
+let devNum;
+const rfid = new readClass();
+let progmode = false;
+let loop;
+sheet.onReady = ()=>{
+  loop =async  function (result){
 
-var hw = new HardwareControl({
-  manufacturer: 'Silicon Labs'
-});
+    setInterval( ( async function() {
+      sheet.getDevNum(devName).then ((value)=>{devNum = value});
 
-hw.on('switchState', (val)=>{
-  console.log(val);
-})
+      let UID = rfid.readCards();
+      if (!UID){
+        console.log("Insert Card");
+        return;
+      }
+      console.log(UID);
+      if (adminUIDs.includes(UID)){
+        progmode = true;
+        console.log('Entered Programming Mode.. please input user card after 3 seconds');
+        console.log ('Note: Programming mode will end in 30 seconds from now.');
+        setTimeout(()=>{ progMode = false;}, 30000);
+      } else if(progmode && !adminUIDs.includes(UID)){
+        await addUser(UID);
+      } 
+      else{
+      let access;
+      await sheet.hasAccess(UID,devNum).then((result)=>{
+        access = result;
+      })
 
-hw.onready = ()=>{
-  hw.mode = 'program';
-
-  setTimeout(()=>{
-    hw.mode = 'enable';
-    hw.sendIpAddress();
-  }, 2000);
+      //console.log("access " + sheet.hasAccess(UID,devNum));
+      // console.log(access);
+      if (sheet.isUser(UID) && access){
+        
+        control.runMachine();
+      } else if (sheet.isUser(UID) && !access){
+          control.stopMachine();
+      } else {
+        console.log("User doesn't exist.");
+        return;
+      }
+      }
+    }),2000)
+  }
+  loop();
 }
+async function addUser(UID){
+  console.log("Initiating add user");
+    sheet.addUser(UID,devNum,devName).then(()=>{
+      progmode = false;
+      clearInterval(loop);
+    });
+  }
+  
---
+++ src/read.js
@@ -0,0 +1,71 @@
+
+const {getDataFromSheet} = require('./spreadsheetChecker');
+const sheet = new getDataFromSheet();
+
+class read{
+    constructor(){
+        // const SoftSPI = require("rpi-softspi");
+        this.time = 30;
+        this.Mfrc522 = require("../index");
+        this.SoftSPI = require("rpi-softspi");
+        this.softSPI = new this.SoftSPI({
+            clock: 23, // pin number of SCLK
+            mosi: 19, // pin number of MOSI
+            miso: 21, // pin number of MISO
+            client: 24 // pin number of CS
+        });
+        this.mfrc522 = new this.Mfrc522(this.softSPI).setResetPin(22).setBuzzerPin(18);
+    }
+
+    readCards(){
+        this.mfrc522.reset();
+        //# Scan for cards
+        let response = this.mfrc522.findCard();
+
+        if (!response.status) {
+        // console.log("No Card");
+        // control.stopMachine();
+        return false;
+        }
+        //# Get the UID of the card
+        response = this.mfrc522.getUid();
+        if (!response.status) {
+        // console.log("UID Scan Error");
+        // control.stopMachine();
+        return false;
+        }
+        //# If we have the UID, continue
+        const uid = response.data;
+        let UID = '' + uid[0].toString(16) + uid[1].toString(16) + uid[2].toString(16)+ uid[3].toString(16);
+        return UID;
+    }
+/*
+    async read2bAddedUser(UID, devNum){
+        let UID2 = this.readCards(); 
+        // console.log(UID + "    --      " + UID2);
+        if (UID2){
+            if (UID2 == UID){
+                console.log('please insert non-admin card');
+                return this.read2bAddedUser(UID);
+            }
+            else{
+                console.log('user captured successfully');
+                return UID2;
+            }  
+        }
+        else {
+            
+            setTimeout(()=>{ 
+                console.log("waiting 3 seconds to re-read the user.");
+                return this.read2bAddedUser();
+                this.time-=3;
+                if (this.time<0){
+                    return;
+                }
+            },3000)}
+        }*/
+    }
+
+
+
+exports.readClass = read;
