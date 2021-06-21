eb1306d3bc06317d59f451c9fa936ba4cea1518b
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-21 14:31:15 -0500

    s

--- Access control/mfrc522-rpi-master/test/controllingMachine.js
+++
@@ -1,9 +0,0 @@
-exports.runMachine = function (){
-     console.log("running");
-};
-exports.stopMachine = function(){
-    console.log('not running');
-};
-exports.noAccess = function(){
-    console.log("to be implemented, you don't have access")
-}
