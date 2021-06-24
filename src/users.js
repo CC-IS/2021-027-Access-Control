345bf8118969a80ab43524d332068274c3742f83
Yusuf Ismail <Ismaily@carleton.edu>
2021-06-24 15:48:46 -0500

    dsd

--- src/users.js
+++ src/users.js
@@ -4,9 +4,10 @@ let updater = new getDataFromSheet();
 
 class Users extends Array {
   constructor(){
-    var _this=this;
+    super();
+    var _this= this;
     updater.onReady(()=>{
-      await update().then(()=>{
+      update().then(()=>{
         _this.onReady();
       });
 
@@ -21,10 +22,13 @@ class Users extends Array {
   async update(){
     var _this = this;
     await updater.getBatch().then((result)=>{
-      let usrs = result.data.values;
+      console.log(result.data.valueRanges[0].values[0]);
+      let usrs = result.data.valueRanges[0].values;
+
       var keys = usrs[0];
-      _this.adminPresent = usrs[1][1];
+      _this.adminPresent =usrs[1][1];
       usrs.slice(2).forEach((row, i) => {
+        _this[i] ={}
         keys.forEach((key, j) => {
           _this[i][key] = row[j];
         });
