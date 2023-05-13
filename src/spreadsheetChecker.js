9a03d2b9aebe21d150b7c00104a950c1fa832b41
Yusuf Ismail <82184077+is-yusuf@users.noreply.github.com>
2023-05-13 13:15:24 -0500

    Update spreadsheetChecker.js

--- src/spreadsheetChecker.js
+++ src/spreadsheetChecker.js
@@ -1,3 +1,8 @@
+/**
+ * @param {string} spreadsheet Id
+ * @param {string} Specefic sheet name
+ * @param {string} Relative path to the JWT token
+ */
 class getDataFromSheet {
 
     constructor(spreadsheetId, sheetName, keyFile) {
@@ -9,8 +14,10 @@ class getDataFromSheet {
         this.usersarr = []
         this.keyFile = keyFile;
     }
-
-
+    /**
+     * 
+     * Gets the new values in the sheet.
+     */
     async update() {
         var _this = this;
         await this.getBatch().then((result) => {
@@ -30,9 +37,19 @@ class getDataFromSheet {
         })
         return;
     }
+
+    /**
+     * 
+     * @param {string} User name/id 
+     * @returns row of the user
+     */
     getUser(UID) {
-        return this.users[this.usersarr.indexOf(UID)];
+        return this.usersarr[this.usersarr.indexOf(UID)];
     }
+    /**
+     * renews the auth with the api
+     * @param {string} keyFile 
+     */
     authorize(keyFile) {
         const { google } = require("googleapis");
         this.googleSheets = google.sheets({ version: "v4", auth: this.client });
@@ -43,14 +60,39 @@ class getDataFromSheet {
         });
         this.client = this.auth.getClient();
     }
+    /**
+     * 
+     * @returns Gets the entire sheet and returns it as an object[id][field] = value
+     */
     async getBatch() {
-        return await this.googleSheets.spreadsheets.values.batchGet({
+        return this.googleSheets.spreadsheets.values.batchGet({
             auth: this.auth,
             spreadsheetId: this.spreadsheetId,
-            ranges: [this.sheetName]
-        })
-    }
+            ranges: `${this.sheetName}!A1:H7`
+        }).then((res) => {
 
+            let values = res.data.valueRanges[0].values;
+            const keys = values[0].slice(1);
+            const objects = {};
+
+            values.slice(1).forEach((row) => {
+                const obj = {};
+                keys.forEach((key, i) => {
+                    obj[key] = row[i + 1];
+                });
+                objects[row[0]] = obj;
+            });
+
+
+            return objects
+        })
+
+    }
+    /**
+     * 
+     * @param {*} A1 
+     * @param {*} value 
+     */
     async changeCell(A1, value) {
         await this.googleSheets.spreadsheets.values.update({
             auth: this.auth,
@@ -70,7 +112,10 @@ class getDataFromSheet {
         } else {
             this.addNewRow(UID).then(() => {
                 console.log(`User ${UID} was added to database. Granting access...`)
-                this.addUser(UID, devName);
+                this.update().then(() => {
+                    this.addUser(UID, devName);
+
+                })
             })
         }
     }
@@ -79,7 +124,7 @@ class getDataFromSheet {
         await this.googleSheets.spreadsheets.values.append({
             auth: this.auth,
             spreadsheetId: this.spreadsheetId,
-            range: `${sheetName}!A:B`,
+            range: `${this.sheetName}!A:B`,
             valueInputOption: "USER_ENTERED",
             resource: {
                 values: [[UID, 0, 0, 0, 0, 0, 0]],
